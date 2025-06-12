23117d80013dee58a87d6226420d852ae8cb334e
Mukund-H-Opash <mha.opash@gmail.com>
2025-06-12 10:14:49 +0530

    folder stracture created

---
+++ froentend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import Navbar from "./Components/Navbar";
+
+export default function Home() {
+  return (
+    <Box sx ={{ backgroundColor: "#000"  ,display: "flex"}}>
+      <Navbar />
+
+    </Box>
+  );
+}
